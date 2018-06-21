9174737fa7174a0147b854b4986a7c08d9e05a69
Alexey Vyskubov <alexey@ocaml.nl>
2018-06-21 16:27:19 +0300

    grains

--- sandhill.py
+++ sandhill.py
@@ -7,6 +7,7 @@ from random import randint
 
 import numpy as np
 import pygame as pg
+import pygame.freetype as ft
 import pygame.locals as lcls
 
 from pygame import surfarray as sf
@@ -18,20 +19,22 @@ HILL = 20
 WAIT = True
 
 EVERY = 12000
-FOR = 120
+FOR = 240
 
 
-def _interesting(idx):
-    return idx % EVERY <= FOR
+def _interesting(num):
+    return num % EVERY <= FOR
 
 
 def _update(field, border, colors):
-    if WAIT and (field < 4).all():
+    added = False
+    if (not WAIT) or (field < 4).all():
         max_val = field.shape[0] - 1 - border
         # add one more
         rand_x = randint(border, max_val)
         rand_y = randint(border, max_val)
         field[rand_x, rand_y] += 1
+        added = True
     # which ones are exploding
     explode = field >= 4
     explode = explode * 1
@@ -46,11 +49,16 @@ def _update(field, border, colors):
     colors[:, :, 1] = (field == 1) * 255 + (field >= 4) * 255
     colors[:, :, 2] = (field == 2) * 255 + (field >= 4) * 255
 
+    return added
 
+
+# pylint:disable=too-many-locals
 def main(size, border, zoom):
     """Entry point."""
     # pylint:disable=no-member
     pg.init()
+
+    font = ft.Font("iosevka-regular.ttf")
 
     field = np.zeros((size, size), dtype=int)
     colors = np.zeros((size, size, 3), dtype=int)
@@ -59,19 +67,16 @@ def main(size, border, zoom):
     surface = pg.Surface((size, size))
     idx = 1
     frame = 1
+    grains = 0
     while True:
         evt = pg.event.poll()
         # pylint:disable=no-member
         # if evt.type == lcls.KEYDOWN and evt.key == lcls.K_SPACE:
-        if _interesting(idx):
-            pg.image.save(surface,
-                          os.path.join("out", "out-{:09d}.png").format(frame))
-            print(".", end='', flush=True)
-            frame += 1
         # pylint:disable=no-member
         if evt.type == lcls.QUIT:
             raise SystemExit()
-        _update(field, border, colors)
+        if _update(field, border, colors):
+            grains += 1
         idx += 1
         if idx % 1000 == 0:
             size2 = size * size
@@ -79,13 +84,18 @@ def main(size, border, zoom):
             red = np.count_nonzero(colors[:, :, 0]) * 100 / size2
             green = np.count_nonzero(colors[:, :, 1]) * 100 / size2
             blue = np.count_nonzero(colors[:, :, 2]) * 100 / size2
-            print("{}: {:.2f}% -> {:.2f}% / {:.2f}% / {:.2f}%".format(idx,
-                                                                      filled,
-                                                                      red,
-                                                                      blue,
-                                                                      green))
-        if _interesting(idx):
+            print("{} ({}): {:.2f}% -> "
+                  "{:.2f}% / {:.2f}% / {:.2f}%".format(idx, grains,
+                                                       filled,
+                                                       red, blue, green))
+        if _interesting(grains):
             sf.blit_array(surface, colors)
+            font.render_to(surface, (10, 10), "{}".format(grains),
+                           fgcolor=(0, 255, 255),
+                           size=12)
+            pg.image.save(surface,
+                          os.path.join("out", "out-{:09d}.png").format(frame))
+            frame += 1
             # pg.transform.smoothscale(surface,
             #                         (size * zoom, size * zoom), screen)
             pg.transform.scale(surface, (size * zoom, size * zoom), screen)
